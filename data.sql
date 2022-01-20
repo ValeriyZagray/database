77647d5834ec2958ee2f9a44cb1b47dbe1d2148c
ValeriyZagray <fsdzvzvzvfsd5@gmail.com>
2022-01-20 15:23:57 +0300

    adding new files

---
+++ data.sql
@@ -0,0 +1,12 @@
+insert into usr (first_name, middle_name, last_name, email) values
+('Сергей', 'Иванович', 'Иванов', 'braseiddammuwa-3088@yopmail.com'),
+('Виктор', 'Иванович', 'Иванов', 'villoppozocreu-9275@yopmail.com'),
+('Глеб', 'Иванович', 'Иванов', 'socegraupetra-9290@yopmail.com'),
+('Дмитрий', 'Иванович', 'Иванов', 'cimmeucegice-5063@yopmail.com'),
+('Андрей', 'Иванович', 'Иванов', 'maddeisaufretta-5926@yopmail.com'),
+('Владимир', 'Иванович', 'Иванов', 'neiddeugrallaro-6593@yopmail.com'),
+('Ольга', 'Иванович', 'Иванов', 'zaffiquetremo-7291@yopmail.com'),
+('Наталья', 'Иванович', 'Иванов', 'pogittequanei-3035@yopmail.com'),
+('Алексей', 'Иванович', 'Иванов', 'woidepeddouyou-1657@yopmail.com'),
+('Всеволод', 'Иванович', 'Иванов', 'bruyehullocra-4737@yopmail.com');
+
